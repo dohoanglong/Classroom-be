ca6b77f139f4e6d354175680c72473a964e259cd
dohoanglong <49149829+dohoanglong@users.noreply.github.com>
2021-11-07 22:55:53 +0700

    fix bug

--- server/config/config.js
+++ server/config/config.js
@@ -2,8 +2,8 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 export default {
-    HOST: process.end.DB_HOST,
-    USER: process.end.DB_USER,
-    PASSWORD: process.end.DB_PASSWORD,
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
     DB: process.env.DB
 };
--- server/models/db.js
+++ server/models/db.js
@@ -1,5 +1,7 @@
 import mysql from 'mysql';
 import dbConfig from '../config/config.js';
+import dotenv from 'dotenv'
+dotenv.config();
 
 // Create a connection to the database
 const connection = mysql.createConnection({
@@ -7,6 +9,10 @@ const connection = mysql.createConnection({
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB
+  // host: process.end.DB_HOST,
+  // user: process.end.DB_USER,
+  // password: process.end.DB_PASSWORD,
+  // database: process.end.DB
 });
 
 // open the MySQL connection
